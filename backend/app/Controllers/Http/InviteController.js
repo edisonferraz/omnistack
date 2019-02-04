079850cdcd1c191339535c33533a873b3d933389
Edison <edisonferraz@gmail.com>
2019-02-04 16:03:29 +0100

    Convites

--- backend/app/Controllers/Http/InviteController.js
+++ backend/app/Controllers/Http/InviteController.js
@@ -1,8 +1,6 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Invite = use('App/Models/Invite')
 
 /**
  * Resourceful controller for interacting with invites
@@ -12,8 +10,16 @@ class InviteController {
    * Create/save a new invite.
    * POST invites
    */
-  async store ({ request, response }) {
-    console.log(request.team)
+  async store ({ request, auth }) {
+    const invites = request.input('invites')
+
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id
+    }))
+
+    await Invite.createMany(data)
   }
 }
 
